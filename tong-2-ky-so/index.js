12421da5496357858b5a379481d8e04884a686c5
Minh <congminh.fe@gmail.com>
2023-08-09 20:07:07 +0700

    commit

--- tong-2-ky-so/index.js
+++ tong-2-ky-so/index.js
@@ -1,3 +1,15 @@
+/* 
+input: 
+gia tri nhap
+
+progress:
+-tinh so hang chuc:
+-tinh so hang tram:
+
+output:
+tong hai so
+**/
+
 document.getElementById('btn-tong').onclick = function(){
     var so = document.getElementById('number').value;
     var hangChuc = Math.floor(so / 10);
